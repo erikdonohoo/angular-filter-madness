0c1fe3fb8880551afed9c0eca8fca0cdc1d91dbc
Erik Donohoo <erikdonohoo@gmail.com>
2014-03-10 09:54:57 -0600

    Added a date search filter

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 angular.module('filterMadness', [])
 .constant('people', [
 	{
-		'name': 'Bobby jo',
+		'name': 'Pistol Pete',
 		'favColor': '#23a8b3',
 		'birthday': 1294160581542,
 		'likeables': ['cat', 'cow', 'bagel', 'cheese']
@@ -17,11 +17,31 @@ angular.module('filterMadness', [])
 		'favColor': '#2bca78',
 		'birthday': 1334160581542,
 		'likeables': ['cat', 'potato', 'seaweed']
+	},
+	{
+		'name': 'Billy Beauregard',
+		'favColor': '#67fe89',
+		'birthday': 1223160581542,
+		'likeables': ['catsup', 'onion', 'bacon']
+	},
+	{
+		'name': 'Hungry Pete',
+		'favColor': '#2647bb',
+		'birthday': 18737160581542,
+		'likeables': ['lance', 'dirt', 'artichoke']
+	},
+	{
+		'name': 'Fantasic Fred',
+		'favColor': '#bc67ca',
+		'birthday': 1309160581542,
+		'likeables': ['catmandu', 'soil', 'shangri la']
 	}
 ])
 .controller('MyCtrl', ['$scope', 'people', function ($scope, people) {
 	this.people = people;
 }])
+
+
 .filter('betterFilter', ['$filter', function ($filter) {
 	return function (list, query) {
 		if (!query)
@@ -43,4 +63,42 @@ angular.module('filterMadness', [])
 
 		return newlist;
 	};
-}]);
+}])
+
+.filter('dateSearch', ['$filter', function ($filter) {
+
+	// List of angular filters to match against
+	var filters = ['medium','short','fullDate','M/d/yyyy', 'MMM yyyy', 'MMMM yyyy', 'longDate', 'MMMM d yyyy'];
+	return function (list, query) {
+
+		if (!query || !list)
+			return list;
+
+		var matches = [];
+
+		// Go through each piece and see if it matches
+		angular.forEach(list, function (item) {
+
+			// Go through each property on item
+			for (prop in item) {
+
+				// If property is date-ish and object has this property
+				if (item.hasOwnProperty(prop) && (angular.isDate(item[prop]) || angular.isNumber(item[prop]))) {
+
+					// Match against filters
+					var match = false;
+					for (var i = 0; i < filters.length; i++) {
+						if (~$filter('date')(item[prop], filters[i]).toLowerCase().indexOf(query.toLowerCase())) {
+							if (!~matches.indexOf(item)) // Make sure we didn't add this item because of some other property
+								matches.push(item);
+							break;
+						}
+					}
+
+				}
+			}
+		});
+
+		return matches;
+	};
+}])
